9d8fbb6efeaf93767b7a2bf006f2eaad0eae03da
Ashton Threadwell <kanyesouthwest@users.noreply.github.com>
2020-02-17 10:27:12 +1300

    Movie Ticket Task

--- Grade Task.py
+++ Grade Task.py
@@ -4,13 +4,16 @@ exam_mark = int(input("Enter exam result: "))
 A = 90
 B = 70
 C = 50
-if exam_mark >= 90:
-    print ("A Grade")
-if exam_mark >= 70 and exam_mark <90:
-    print ("B Plus")
-if exam_mark >= 50 and exam_mark <69:
-    print ("C Minus")
-if exam_mark == 69:
-    print ("Nice you got a C+")
-if exam_mark <50:
-    print ("Sorry fam you failed thine test")
+if exam_mark >100 and exam_mark <0: 
+    if exam_mark >= A:
+        print ("A Grade")
+    if exam_mark >= B and exam_mark <A:
+        print ("B Plus")
+    if exam_mark >= C and exam_mark <69:
+        print ("C Minus")
+    if exam_mark == 69:
+        print ("Nice you got a C+")
+    if exam_mark <50:
+        print ("Sorry fam you failed thine test")    
+else: 
+    print("Thats the wrong number")
---
+++ Ticket Price.py
@@ -0,0 +1,9 @@
+EIGHTEEN_YEARS_OR_OLDER = 12
+THIRTEEN_YEARS_OR_OLDER = 9
+FIVE_YEARS_OR_OLDER = 7
+
+age = int(input("How old are you chief: "))
+student = (input("Are you a student: "))
+
+if student == "yes": and student >= 13:  
+    print("Congrats your ticket is $8")
